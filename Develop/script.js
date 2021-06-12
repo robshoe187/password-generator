1a520eebc362648488736e7f2f135f4edc3d6c24
Robert Shoemaker <rob.shoe187@gmail.com>
2021-06-12 05:18:20 -0400

    added password length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,32 @@
 // Assignment code here
 
+function passwordSizeEntry() {
+  var passSize = parseInt(window.prompt("What is the length of your password? (8-128 Characters)"));
+
+  if (passSize < 8) {
+    window.alert("Password too short please choose another length.");
+    passwordSizeEntry();
+  }
+  else if (passSize > 128) {
+    window.alert("Password too long please choose another length.");
+    passwordSizeEntry();
+  }
+  else if(typeof passSize !== "number") {
+    window.alert("Password incorrect please enter another value.");
+    passwordSizeEntry();
+  }
+  else {
+    passPrompts();
+  }
+}
+
+function passPrompts() {
+  console.log("passPrompts");
+}
+
+function generatePassword() {
+  passwordSizeEntry();
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
